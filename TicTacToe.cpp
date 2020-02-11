463cf9dbe13e33c58d78eb8a1b6be4856acc0f3d
Yang Li <851190521@qq.om>
2020-02-11 15:43:14 -0700

    Resolve conflict

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -3,6 +3,18 @@
 void CreateBoard(){
     int board[3][3];
 }
+
+void DisplayBoard(int arr_){
+	for (int i = 0; i < 3; ++i)
+	{
+		for (int j = 0; j < 3; ++j)
+		{
+			std::cout << arr_[i][j];
+		}
+		std::cout<<std::endl;
+	}
+}
+
 int main(){
     CreateBoard();
 }
