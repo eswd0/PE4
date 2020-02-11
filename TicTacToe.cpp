b51d0290613ac5b5de74a1a173db8c176c782e41
eswd0 <jili6190@colorado.edu>
2020-02-11 16:17:37 -0700

    TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <vector>
 
-void CreateBoard(){
-    int board[3][3];
+std::vector<int> CreateBoard(){
+    std::vector<int> board;
+    for(int i=0;i<9;i++){
+        board.push_back(0);
+    }
+    return board;
 }
 
-void DisplayBoard(int arr_){
-	for (int i = 0; i < 3; ++i)
-	{
-		for (int j = 0; j < 3; ++j)
-		{
-			std::cout << arr_[i][j];
-		}
-		std::cout<<std::endl;
-	}
+void DisplayBoard(std::vector<int> board){
+    for (int i = 0; i < board.size(); ++i){
+        std::cout << board[i]<<"|";
+        if(i%3==2){
+            std::cout<<std::endl<<"------"<<std::endl;
+        }
+    }
 }
 
 int main(){
-    //CreateBoard();
-    CreateBoard();
+    DisplayBoard(CreateBoard());
 }
